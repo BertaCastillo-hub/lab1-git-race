46169f8a06058625b9d1ea1f4865ff1db2057022
Berta Castillo Crespo <897199@unizar.es>
2026-09-17 18:25:14 +0000

    fix compilation errors on filter

--- src/main/kotlin/filter/RateLimiterConfig.kt
+++ src/main/kotlin/filter/RateLimiterConfig.kt
@@ -5,6 +5,7 @@ import io.github.bucket4j.caffeine.CaffeineProxyManager
 import io.github.bucket4j.distributed.proxy.AsyncProxyManager
 import io.github.bucket4j.distributed.remote.RemoteBucketState
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions
 import org.springframework.cloud.gateway.server.mvc.filter.Bucket4jFilterFunctions.rateLimit
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http
@@ -32,12 +33,18 @@ class RateLimiterConfig {
     // Declarative configuration of the route with the RateLimiter filter integrated
     @Bean
     fun gatewayRouter(): RouterFunction<ServerResponse> {
-        // Exclude /actuator/** from rate limiting
-        val rateLimitedPaths = RequestPredicates.path("/**")
+        // Intercept GET requests, excluding /actuator/** and requests that have 
+        // already passed through the Gateway, to avoid loops
+        val incomingRequests = RequestPredicates.GET("/**")
+            .and(RequestPredicates.headers { !it.header("X-Gateway-Proxied").contains("true") })
             .and(RequestPredicates.path("/actuator/**").negate())
 
         return route("rate_limit_route")
-            .GET("/**", http())
+            .route(incomingRequests, http())
+            // Add a header to know that the request already passed through the rate limit filter
+            .before(BeforeFilterFunctions.addRequestHeader("X-Gateway-Proxied", "true"))
+            // Destination URI
+            .before(BeforeFilterFunctions.uri("http://localhost:8080"))
             .filter(
                 rateLimit { config ->
                     config.setCapacity(10)
